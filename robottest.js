9579340f9494e63fe32a7dbd6183ab58b5e04f3b
David Ko <david.dk.ko@gmail.com>
2016-03-16 15:41:27 -0700

    Added semicolon to demo program

--- robottest.js
+++ robottest.js
@@ -5,7 +5,7 @@ var robot = new robotimpl.RobotImpl();
 var a = 90*3.14/180;
 
 robot.connect('ws://localhost:42000', 'DGKR').then(function() {
-    return robot.move(a,a,a,7)
+    return robot.move(a,a,a,7);
 }).then(function() {
     return robot.moveWait(7);
 }).then(function() {
